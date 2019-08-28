30e7d65985d5fd6fa322d63a49e1470b2e5911e8
toby.henderson <toby.henderson@huddle.com>
2019-08-28 16:36:33 +0100

    Fix darker registrations, allowing for scope lifetime

--- src/ToDoApi/Startup.cs
+++ src/ToDoApi/Startup.cs
@@ -16,6 +16,7 @@ using Polly;
 using Polly.Registry;
 using ToDoCore.Adaptors.Db;
 using ToDoCore.Ports.Queries;
+using Constants = Paramore.Darker.Policies.Constants;
 
 namespace ToDoApi
 {
@@ -33,8 +34,8 @@ namespace ToDoApi
         {
             services.AddDbContext<ToDoContext>(options =>
                 options.UseNpgsql("Host=localhost;Database=ToDoDB;Username=postgres;Password=password"));
-            
-            var rmqMessagingGatewayConnection = new RmqMessagingGatewayConnection()
+
+            var rmqMessagingGatewayConnection = new RmqMessagingGatewayConnection
             {
                 Name = nameof(ToDoApi),
                 AmpqUri = new AmqpUriSpecification(new Uri("amqp://guest:guest@localhost:5672")),
@@ -48,48 +49,54 @@ namespace ToDoApi
             });
             var asyncCircuitBreakerPolicy = Policy.Handle<Exception>().CircuitBreakerAsync(10, new TimeSpan(5000));
 
-            var brighterPolicy = new Polly.Registry.PolicyRegistry
+            var brighterPolicy = new PolicyRegistry
             {
-                { CommandProcessor.CIRCUITBREAKER, Policy.Handle<Exception>().CircuitBreaker(10, new TimeSpan(5000)) },
-                { CommandProcessor.CIRCUITBREAKERASYNC, asyncCircuitBreakerPolicy },
-                { CommandProcessor.RETRYPOLICY,
+                {CommandProcessor.CIRCUITBREAKER, Policy.Handle<Exception>().CircuitBreaker(10, new TimeSpan(5000))},
+                {CommandProcessor.CIRCUITBREAKERASYNC, asyncCircuitBreakerPolicy},
+                {
+                    CommandProcessor.RETRYPOLICY,
                     Policy.Handle<Exception>().WaitAndRetry(new[]
                     {
                         TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(100),
                         TimeSpan.FromMilliseconds(150)
                     })
                 },
-                { CommandProcessor.RETRYPOLICYASYNC, asyncRetryPolicy },
-                { Paramore.Darker.Policies.Constants.RetryPolicyName, asyncRetryPolicy},
-                { Paramore.Darker.Policies.Constants.CircuitBreakerPolicyName, asyncCircuitBreakerPolicy}
+                {CommandProcessor.RETRYPOLICYASYNC, asyncRetryPolicy},
+                {Constants.RetryPolicyName, asyncRetryPolicy},
+                {Constants.CircuitBreakerPolicyName, asyncCircuitBreakerPolicy}
             };
 
-            
+
             services.AddBrighter(options =>
-            {
-                options.BrighterMessaging = new BrighterMessaging(new InMemoryMessageStore(), new RmqMessageProducer(rmqMessagingGatewayConnection));
-                options.PolicyRegistry = brighterPolicy;
-                //EFCore by default registers Context as scoped, which forces the CommandProcessorLifetime to also be scoped
-                options.CommandProcessorLifetime = ServiceLifetime.Scoped;
-            }).AutoFromAssemblies();
-            
-            services.AddDarker()
+                {
+                    options.BrighterMessaging = new BrighterMessaging(new InMemoryMessageStore(), new RmqMessageProducer(rmqMessagingGatewayConnection));
+                    options.PolicyRegistry = brighterPolicy;
+                    //EFCore by default registers Context as scoped, which forces the CommandProcessorLifetime to also be scoped
+                    options.CommandProcessorLifetime = ServiceLifetime.Scoped;
+                }).AutoFromAssemblies();
+
+            services.AddDarker(options =>
+                {
+                    //EFCore by default registers Context as scoped, which forces the QueryProcessorLifetime to also be scoped
+                    options.QueryProcessorLifetime = ServiceLifetime.Scoped;
+                })
                 .AddHandlersFromAssemblies(typeof(ToDoByIdQuery).Assembly)
                 .AddPolicies(brighterPolicy)
                 .AddJsonQueryLogging();
-            
+
             services.AddMvc();
-            
+
             services.AddCors(options =>
             {
-                options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials()
+                options.AddPolicy("AllowAll",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
                 );
             });
 
-            services.Configure<MvcOptions>(options => {
+            services.Configure<MvcOptions>(options =>
+            {
                 options.Filters.Add(new CorsAuthorizationFilterFactory("AllowAll"));
             });
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
@@ -102,10 +109,8 @@ namespace ToDoApi
 
             app.UseCors("AllowAll");
             app.UseMvc();
-            
+
             context.Database.EnsureCreated();
         }
     }
 }
-
-
